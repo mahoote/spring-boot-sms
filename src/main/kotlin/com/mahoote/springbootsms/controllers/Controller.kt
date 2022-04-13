da4785a069f7fe6859ff4384a66795097b339cf2
Martin Teigen <martin@teigen.biz>
2022-04-13 21:46:31 +0200

    Get test

--- src/main/kotlin/com/mahoote/springbootsms/controllers/Controller.kt
+++ src/main/kotlin/com/mahoote/springbootsms/controllers/Controller.kt
@@ -4,14 +4,16 @@ import com.mahoote.springbootsms.models.SmsRequest
 import com.mahoote.springbootsms.service.Service
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api/v1/sms")
 class Controller(@Autowired private val service: Service) {
+
+    @GetMapping
+    fun testGet(): String {
+        return "It worked!"
+    }
 
     @PostMapping
     fun sendSms(@Validated @RequestBody smsRequest: SmsRequest) {
