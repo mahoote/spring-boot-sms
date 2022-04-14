d8f243a41365a6d86e001020e3e70dc36330e08b
Martin Teigen <martin@teigen.biz>
2022-04-14 02:00:30 +0200

    Add dummy receiveSms function with request param

--- src/main/kotlin/com/mahoote/springbootsms/controllers/Controller.kt
+++ src/main/kotlin/com/mahoote/springbootsms/controllers/Controller.kt
@@ -3,6 +3,7 @@ package com.mahoote.springbootsms.controllers
 import com.mahoote.springbootsms.models.SmsRequest
 import com.mahoote.springbootsms.service.Service
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
@@ -16,7 +17,7 @@ class Controller(@Autowired private val service: Service) {
         return "It worked!"
     }
 
-    @PostMapping("/receive")
+    @PostMapping(value = ["/receive"], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE], produces = [MediaType.APPLICATION_ATOM_XML_VALUE])
     fun receiveSms(@RequestBody request: HttpServletRequest) {
         service.receiveSms(request)
     }
