c3b23accc1470d9616740a27cc35b8c18c92f11f
Martin Teigen <martin@teigen.biz>
2022-04-15 20:30:48 +0200

    Remove user init data

--- src/main/resources/db/migration/V002__initial_data.sql
+++ src/main/resources/db/migration/V002__initial_data.sql
@@ -2,6 +2,6 @@ insert into questions (question, key_word)
 values ('Hei, Martin!', 'Hei'),
        ('Det går fint, takk som spør', 'Hvordan går det?');
 
-insert into users (phone_nr)
-values ('+4795040924'),
-       ('+4797769260');
+-- insert into users (phone_nr)
+-- values ('+4795040924'),
+--        ('+4797769260');
